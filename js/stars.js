ea0c5cd1421d6a83d2f1b0acacd67932797aaa9d
spsanps <sanjayanps@gmail.com>
2016-06-23 14:58:43 +0400

    More Stars!

--- js/stars.js
+++ js/stars.js
@@ -2,15 +2,15 @@
  * Created by sanps on 22-Jun-16.
  */
 
-function drawing() {
-    var c = document.getElementById('stars');
+function drawing(id, reduceAmount) {
+    var c = document.getElementById(id);
     var ctx = c.getContext('2d');
     var xMax = c.width = window.screen.availWidth;
     var yMax = c.height = window.screen.availHeight;
 
     var hmTimes = Math.round(xMax + yMax);
 
-    for (var i = 0; i <= hmTimes / 15; i++) {
+    for (var i = 0; i <= hmTimes / reduceAmount; i++) {
         var randomX = Math.floor((Math.random() * xMax) + 1);
         var randomY = Math.floor((Math.random() * yMax) + 1);
         var randomSize = Math.floor((Math.random() * 3) + 1);
@@ -29,4 +29,8 @@ function drawing() {
     }
 
 }
-drawing();
+drawing('stars1', 15);
+drawing('stars2', 25);
+drawing('stars3', 45);
+drawing('stars4', 85);
+drawing('stars5', 150);
